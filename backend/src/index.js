517e2c6730d99077a2b793c77558af59103dc533
Diego Clair <diego93rodrigues@gmail.com>
2020-03-26 00:54:51 -0300

    Added log message from port listener

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,14 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
+const port = 3333;
 
 app.use(cors());
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
